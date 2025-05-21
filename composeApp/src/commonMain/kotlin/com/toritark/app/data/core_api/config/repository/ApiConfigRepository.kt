13b97204d6ae1ede0b648be0f2e00adf7a82d739
Artem Smirnov <artem@smirnov.page>
2025-05-21 19:32:08 +0300

    Get API configuration from BuildKonfig

--- composeApp/src/commonMain/kotlin/com/toritark/app/data/core_api/config/repository/ApiConfigRepository.kt
+++ composeApp/src/commonMain/kotlin/com/toritark/app/data/core_api/config/repository/ApiConfigRepository.kt
@@ -1,5 +1,6 @@
 package com.toritark.app.data.core_api.config.repository
 
+import com.toritark.app.BuildKonfig
 import com.toritark.app.data.core_api.config.model.ApiConfig
 
 
@@ -11,12 +12,9 @@ internal class ApiConfigRepositoryImpl : ApiConfigRepository {
 
     override suspend fun getApiConfig(): ApiConfig {
         return ApiConfig(
-            host = "192.168.1.3",
-            port = 8000,
-            isHttps = false,
-//            host = "api.staging.actinis.io",
-//            port = 443,
-//            isHttps = true,
+            host = requireNotNull(BuildKonfig.API_HOST.takeIf { it.isNotBlank() }) { "API Host is not set" },
+            port = BuildKonfig.API_PORT,
+            isHttps = BuildKonfig.API_IS_HTTPS,
         )
     }
 }
