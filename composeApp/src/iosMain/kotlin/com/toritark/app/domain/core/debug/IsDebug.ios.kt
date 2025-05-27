4172c6c507e5e0194f6445dd9014818a4a67f44f
Artem Smirnov <artem@smirnov.page>
2025-05-28 00:04:44 +0300

    Implement IsDebug on iOS

--- composeApp/src/iosMain/kotlin/com/toritark/app/domain/core/debug/IsDebug.ios.kt
+++ composeApp/src/iosMain/kotlin/com/toritark/app/domain/core/debug/IsDebug.ios.kt
@@ -1,10 +1,13 @@
 @file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+@file:OptIn(ExperimentalNativeApi::class)
 
 package com.toritark.app.domain.core.debug
+
+import kotlin.experimental.ExperimentalNativeApi
 
 internal actual class IsDebugImpl : IsDebug {
 
     actual override fun invoke(): Boolean {
-        return true // FIXME
+        return Platform.isDebugBinary
     }
 }
