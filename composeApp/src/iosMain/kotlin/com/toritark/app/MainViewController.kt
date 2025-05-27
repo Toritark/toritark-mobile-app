695608006295e80578bd22607ab1dd27af56f9a5
Artem Smirnov <artem@smirnov.page>
2025-05-27 06:28:12 +0300

    Fix iOS Compose app function

--- composeApp/src/iosMain/kotlin/com/toritark/app/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/toritark/app/MainViewController.kt
@@ -1,5 +1,11 @@
 package com.toritark.app
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.toritark.app.presentation.main.app.MainApp
+import com.toritark.app.presentation.main.app.theme.AppTheme
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    AppTheme {
+        MainApp()
+    }
+}
