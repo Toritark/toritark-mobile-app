b746e518ca3f97b5d21dcb924f7a076b5657bb52
Artem Smirnov <artem@smirnov.page>
2025-05-30 22:36:57 +0300

    Update code2prompt

--- scripts/code2prompt.sh
+++ scripts/code2prompt.sh
@@ -1,3 +1,40 @@
 #!/bin/sh
 
-code2prompt . --exclude "README.md,LICENSE.txt,*.json,*.xml,*.png,*.html,*.css,*.properties,*.jar,*.bat,*.sh,*.xcassets,*.xcodeproj,*.pbxproj,*.plist,gradlew,drawable*,mipmap*,*.webp,*/build*,*Test.kt,AllLanguages.kt" --tokens format --output-file "$1"
+# shellcheck disable=SC3030
+exclude_items=(
+    "README.md"
+    "LICENSE.txt"
+    "*.json"
+    "*.xml"
+    "*.png"
+    "*.html"
+    "*.css"
+    "*.properties"
+    "*.jar"
+    "*.bat"
+    "*.sh"
+    "*.xcassets"
+    "*.xcodeproj"
+    "*.pbxproj"
+    "*.plist"
+    "gradlew"
+    "drawable*"
+    "mipmap*"
+    "*.webp"
+    "*/build*"
+    "*Test.kt"
+    "*androidUnitTest*"
+    "AllLanguages.kt"
+    "*/composeResources*"
+    "*wasmJsMain*"
+    "*iosLibs.versions.toml"
+    "*img/*"
+    "iosApp/*"
+    "*data/analytics/*"
+    "*di/module/*"
+)
+
+# shellcheck disable=SC3054
+exclude_string=$(IFS=,; echo "${exclude_items[*]}")
+
+code2prompt . --exclude "$exclude_string" --tokens format --no-clipboard --output-file "$1"
